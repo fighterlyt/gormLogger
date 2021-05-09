0f0a1ea3e68b89f7a571194c3539cc7cc190cebf
fighterlyt <fighter.lyt@gmail.com>
2021-05-09 14:24:06 +0400

    添加注释

--- logger.go
+++ logger.go
@@ -22,6 +22,13 @@ type Logger struct {
 	slowThreshold time.Duration // 慢查询耗时阈值
 }
 
+/*NewLogger 构建日志器
+参数:
+*	originLogger    	log.Logger      	原始日志器，不能为nil
+*	slowThreshold   	time.Duration   	慢查询阈值
+返回值:
+*	logger.Interface	logger.Interface	gorm日志器
+*/
 func NewLogger(originLogger log.Logger, slowThreshold time.Duration) logger.Interface {
 	return &Logger{Logger: originLogger, slowThreshold: slowThreshold}
 }
@@ -45,18 +52,47 @@ func (l *Logger) LogMode(level logger.LogLevel) logger.Interface {
 	return l
 }
 
+/*Info  日志
+参数:
+*	_	context.Context	上下文
+*	s	string          内容
+*	i	...interface{} 	数据
+返回值:
+*/
 func (l Logger) Info(_ context.Context, msg string, data ...interface{}) {
 	l.Logger.Info(fmt.Sprintf(msg, data...))
 }
 
+/*Warn  警告
+参数:
+*	_	context.Context	上下文
+*	s	string          内容
+*	i	...interface{} 	数据
+返回值:
+*/
 func (l Logger) Warn(_ context.Context, s string, i ...interface{}) {
 	l.Logger.Warn(s, zap.Any(`值`, append([]interface{}{utils.FileWithLineNum()}, i...)))
 }
 
+/*Error  错误
+参数:
+*	_	context.Context	上下文
+*	s	string          内容
+*	i	...interface{} 	数据
+返回值:
+*/
 func (l Logger) Error(_ context.Context, s string, i ...interface{}) {
 	l.Logger.Error(s, zap.Any(`值`, append([]interface{}{utils.FileWithLineNum()}, i...)))
 }
 
+/*Trace 追踪
+参数:
+*	_    	context.Context       	上下文
+*	begin	time.Time             	开始时间
+*	fc   	func() (string, int64)	返回sql语句和影响行数
+*	err  	error                 	执行过程中是否报错
+返回值:
+*/
 func (l Logger) Trace(_ context.Context, begin time.Time, fc func() (string, int64), err error) {
 	elapsed := time.Since(begin)
 	sql, rows := fc()
@@ -66,7 +102,6 @@ func (l Logger) Trace(_ context.Context, begin time.Time, fc func() (string, int
 		l.Logger.Error(`执行错误`, zap.String(`错误`, err.Error()), zap.Int64(`影响行数`, rows), zap.Duration(`耗时`, elapsed), zap.String(sqlField, sql))
 	case elapsed > l.slowThreshold && l.slowThreshold != 0:
 		l.Logger.Warn(`慢查询`, zap.Duration(`阈值`, l.slowThreshold), zap.Int64(`影响行数`, rows), zap.Duration(`耗时`, elapsed), zap.String(sqlField, sql))
-
 	default:
 		l.Logger.Info(`执行成功`, zap.Int64(`影响行数`, rows), zap.Duration(`耗时`, elapsed), zap.String(sqlField, sql))
 	}
